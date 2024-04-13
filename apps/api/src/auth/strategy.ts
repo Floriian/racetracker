635a786bbb1573b971bf7f89a51d3eb69c70ad91
Floriian <lovasifloriian@outlook.hu>
2024-04-13 15:20:24 +0200

    handle jwt error.

--- apps/api/src/auth/strategy.ts
+++ apps/api/src/auth/strategy.ts
@@ -1,23 +1,28 @@
 import { Strategy } from 'passport-http-bearer';
-import jwt from 'jsonwebtoken';
+import jwt, { JsonWebTokenError, JwtPayload } from 'jsonwebtoken';
 import { prisma } from '../database';
-import { JwtPayload } from '../types';
+import { JwtPayload as CustomPayload } from '../types';
 
 export const strategy = new Strategy(
   async (token: string, done: (...args: any) => void) => {
     try {
-      const verifyToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+      const verifyToken = jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET
+      ) as unknown as JwtPayload & CustomPayload;
       if (!verifyToken) return done(verifyToken);
 
-      const decodedToken = jwt.decode(token) as unknown as JwtPayload;
       const user = await prisma.user.findFirst({
-        where: { email: decodedToken.email },
+        where: { email: verifyToken.email },
       });
+
       if (!user) return done(null, false);
       delete user.password;
       return done(null, user, { scope: 'all' });
     } catch (e) {
-      return done(e);
+      if (e instanceof JsonWebTokenError) {
+        return done(null, { message: 'Invalid token.' });
+      }
     }
   }
 );
